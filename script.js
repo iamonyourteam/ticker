c168e84f48a999e9570dad6cc0e3f090130419f4
iamonyourteam <jtneff92@gmail.com>
2022-07-15 19:35:57 -0400

    Proxy
    Added Proxy  for fetch to be able to run locally

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-function pageScroll() {
-  document.querySelector('#ticker').scrollBy(2,0);
-  scrolldelay = setTimeout(pageScroll,10);
-}
+
+fetch("https://scores.nbcsports.com/ticker/data/gamesNEW.js.asp?sport=MLB&period=20220509&jsonp=true&random=1652230716683")
+.then(response => response.text())
+.then(function(html) {
+  // Initialize the DOM parser
+  var parser = new DOMParser();
+
+  // Parse the text
+  var doc = parser.parseFromString(html, "text/html");
+
+  // You can now even select part of that html as you would in the regular DOM 
+  // Example:
+  var docArticle = doc.querySelectorAll('ticker-entry')[10].innerHTML;
+
+  console.log(doc);
+  console.log(docArticle);
+
+})
+
